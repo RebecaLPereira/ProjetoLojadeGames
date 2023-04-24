6b488baa1a6825ac5eea5e376134eb406dd901db
RebecaLPereira <leitepereira.rebeca@gmail.com>
2023-04-24 13:02:54 -0300

    Upload do projeto final 1.0

---
+++ lojadegames/src/main/java/com/generation/lojadegames/model/Categoria.java
@@ -0,0 +1,55 @@
+package com.generation.lojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String categoria;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoriaProduto", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoriaProduto")
+	private List<Produto> listaProdutos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Produto> getListaProdutos() {
+		return listaProdutos;
+	}
+
+	public void setListaProdutos(List<Produto> listaProdutos) {
+		this.listaProdutos = listaProdutos;
+	}
+}
---
+++ lojadegames/src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,65 @@
+package com.generation.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@Size(min = 1, max = 5, message = "O atributo avaliação deve estar entre 1 a 5")
+	private int avaliacao;
+
+	@ManyToOne
+	@JsonIgnoreProperties("listaProdutos")
+	private Categoria categoriaProduto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(int avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public Categoria getCategoriaProduto() {
+		return categoriaProduto;
+	}
+
+	public void setCategoriaProduto(Categoria categoriaProduto) {
+		this.categoriaProduto = categoriaProduto;
+	}
+
+}
